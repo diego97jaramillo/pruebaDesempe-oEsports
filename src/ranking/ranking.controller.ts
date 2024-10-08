8929693d6426d73ae71e168ab7a6746efbfa7b21
diego97jaramillo <diego97jaramillo@gmail.com>
2024-10-07 21:45:52 -0500

    feat: we created the documentation Readme.md son anyone can execute our program

--- src/ranking/ranking.controller.ts
+++ src/ranking/ranking.controller.ts
@@ -3,33 +3,40 @@ import { RankingService } from './ranking.service';
 import { CreateRankingDto } from './dto/create-ranking.dto';
 import { UpdateRankingDto } from './dto/update-ranking.dto';
 import { xApiKey } from 'src/common/guards/x-api.guard';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Ranking Api")
 @Controller('ranking')
 export class RankingController {
   constructor(private readonly rankingService: RankingService) {}
 
   @Post()
+  @ApiOperation({ summary: 'create one ranking' })
   create(@Body() createRankingDto: CreateRankingDto) {
     return this.rankingService.create(createRankingDto);
   }
 
   @UseGuards(xApiKey)
   @Get()
+  @ApiOperation({ summary: 'find all rankings' })
   findAll() {
     return this.rankingService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'find one ranking' })
   findOne(@Param('id') id: string) {
     return this.rankingService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'update one ranking' })
   update(@Param('id') id: string, @Body() updateRankingDto: UpdateRankingDto) {
     return this.rankingService.update(id, updateRankingDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'delete one ranking' })
   remove(@Param('id') id: string) {
     return this.rankingService.remove(id);
   }
